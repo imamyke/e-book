a7b905334b7074c1d1cf02346fcf9399fc515965
ImAmyKe <xiahamy@gmail.com>
2023-08-19 02:57:07 +0800

    字體、字體大小選擇、語言切換、閱讀進度、閱讀時間

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/styles/icon.css'
+import './assets/fonts/cabin.css'
+import './assets/fonts/daysOne.css'
+import './assets/fonts/montserrat.css'
+import './assets/fonts/tangerine.css'
 
 createApp(App).use(store).use(router).mount('#app')
